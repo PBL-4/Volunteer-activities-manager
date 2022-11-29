7b6db0f22a8e8043e7a8595a9ec936a49367977a
Bach IT <letrongbach02@gmail.com>
2022-11-29 16:02:19 +0700

    BachLT

--- demo1_pbl4/src/main/java/com/example/demo1_pbl4/controller/PostController.java
+++ demo1_pbl4/src/main/java/com/example/demo1_pbl4/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.example.demo1_pbl4.controller;
 
+import com.example.demo1_pbl4.model.Post;
 import com.example.demo1_pbl4.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,17 +8,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("posts")
+@RequestMapping("/posts")
 public class PostController {
     @Autowired
     private PostService postService;
 
-    @GetMapping("/get{postId}")
-    public String showPostById(Model model, @PathVariable ("postId") Long id)
-    {
+    @GetMapping("")
+    public ModelAndView showAllPost(){
+        return new ModelAndView("/post/post_list","posts",postService.getAllPosts());
+    }
 
-        return "/post/show_post";
+    @GetMapping("/get{id}") // ko co dau .  , dau ? thanh %20
+    public String showPostById(Model model, @RequestParam("id") Long id) // Tai sao lai dung @requestParam
+    {
+        Post post=postService.getPostById(id);
+        System.out.println("Hello");
+        model.addAttribute("post",post);
+        return "/post/post_each_event";
     }
 }
